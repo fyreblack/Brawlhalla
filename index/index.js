ade49631dc7b2a9832de42bd9dd893c243f9dee2
Yujen Maharjan <121010820+YujenM@users.noreply.github.com>
2023-09-09 18:10:20 +0545

    website made

---
+++ index/index.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+    $('.owl-carousel').owlCarousel({
+        items: 1, // Number of items to display
+        // loop: true, // Disable loop
+        margin: 10, // Item margin
+        nav: true, // Enable navigation buttons
+        dots: true, // Enable navigation dots
+        // autoplay: true, // Enable autoplay
+        // autoplayTimeout: 5000, // Autoplay interval in milliseconds
+        responsive: {
+        0: {
+            items: 1 // Number of items to display in small screens
+        },
+        600: {
+            items: 2 // Number of items to display in medium screens
+        },
+        1000: {
+            items: 3 // Number of items to display in large screens
+        }
+        }
+    });
+    });
+
+    // let countdown=new Date("02:00:00").getTime();
+    // let x=setInterval(function(){
+    //     let now=new Date().getTime();
+    //     let distance=countdown-now;
+    //     // let days=Math.floor(distance/(1000*60*60*24));
+    //     let hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    //     let minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+    //     let seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
+    //     document.getElementById("hours").innerHTML=h;
+    //     document.getElementById("minutes").innerHTML=minutes;
+    //     document.getElementById("second").innerHTML=seconds
+    // },1000);
+
+// Get the current date
+// Get the current date
+// Get the current date
+// Get the current date
+
+
+
+
+
+
+    
